76f13f92ddc5dabb45e559c44b4c606b0acca696
Parth Arora <105902832+NinjaCoder3@users.noreply.github.com>
2022-05-20 12:21:49 +0530

    Time: 122 ms (54.52%), Space: 19.9 MB (96.49%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -7,22 +7,25 @@ public:
         return {};
     }
     sort(ar.begin(), ar.end());
-    int n = ar.size();
+    int ei = ar.size() - 1;
+    int si;
     vector<vector<int>> ans;
-    for (int i = 0; i < n - 2; i++)
+    for (int i = 0; i < ar.size() - 2; i++)
     {
+        // remove duplicates from left side         
         if(i > 0 && ar[i] == ar[i - 1])
         {
             continue;
         }
-        int si = i + 1;
-        int ei = n - 1;
+        si = i + 1;
+        ei = ar.size() - 1;
         while (si < ei)
         {
             if (ar[i] + ar[si] + ar[ei] == 0)
             {
                 ans.push_back({ar[i], ar[si], ar[ei]});
                 ei--;
+                // remove duplicated from right side                 
                 while (si < ei && ar[ei] == ar[ei + 1])
                 {
                     ei--;
