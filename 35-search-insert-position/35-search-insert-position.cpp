a50a53b46f358b42a64ced4256f73816890976c3
Parth Arora <105902832+NinjaCoder3@users.noreply.github.com>
2022-05-21 17:16:16 +0530

    Time: 10 ms (22.49%), Space: 9.7 MB (23.62%) - LeetHub

---
+++ 35-search-insert-position/35-search-insert-position.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int searchInsert(vector<int> &ar, int target)
+{
+    int i = 0, j = ar.size() - 1, mid, ans = -1;
+    while (i <= j)
+    {
+        mid = i + (j - i) / 2;
+        if (ar[mid] == target)
+        {
+            return mid;
+        }
+        else if (target > ar[mid])
+        {
+            ans = mid + 1; // possible ans can be just next index
+            i = mid + 1;   // move right
+        }
+        else
+        {
+            ans = mid;   // possible ans can be the same index
+            j = mid - 1; // move left
+        }
+    }
+    return ans;
+}
+};
