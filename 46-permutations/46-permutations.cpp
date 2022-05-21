d00947971a38d40d5fe6b39cb356583298fa7184
Parth Arora <105902832+NinjaCoder3@users.noreply.github.com>
2022-05-21 19:02:55 +0530

    Time: 2 ms (79.97%), Space: 7.8 MB (46.94%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
-    
-void helper(vector<int> &ar, int si, int ei, vector<int> &ans, vector<vector<int>> &v)
+    void helper(vector<int> &ar, int si, int ei, vector<int> &ans, vector<vector<int>> &v, vector<int> &visited)
 {
     if (ans.size() == ar.size())
     {
@@ -11,11 +10,14 @@ void helper(vector<int> &ar, int si, int ei, vector<int> &ans, vector<vector<int
 
     for (int i = si; i <= ei; i++)
     {
-        swap(ar[si], ar[i]);
-        ans.push_back(ar[si]);
-        helper(ar, si + 1, ei, ans, v);
-        swap(ar[si], ar[i]);
-        ans.pop_back();
+        if (visited[i] == 0)
+        {
+            visited[i] = 1;
+            ans.push_back(ar[i]);
+            helper(ar, si, ei, ans, v, visited);
+            visited[i] = 0;
+            ans.pop_back();
+        }
     }
 }
 
@@ -23,7 +25,8 @@ vector<vector<int>> permute(vector<int> &nums)
 {
     vector<int> ans;
     vector<vector<int>> v;
-    helper(nums, 0, nums.size() - 1, ans, v);
+    vector<int> visited(10, 0); // as mentioned in the questation that -10 <= nums[i] <= 10
+    helper(nums, 0, nums.size() - 1, ans, v, visited);
     return v;
 }
 };
