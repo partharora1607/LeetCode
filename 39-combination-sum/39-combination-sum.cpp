77601af345a4c795e92ff07e4c3bcceaf3b999c8
Parth Arora <105902832+NinjaCoder3@users.noreply.github.com>
2022-05-21 17:42:13 +0530

    Time: 19 ms (41.82%), Space: 16.9 MB (37.93%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+void helper(vector<int> &ar, int si, int ei, vector<int> &ans, int target, int sum, vector<vector<int>>& v)
+{
+    if (si > ei)
+    {
+        if (sum == target)
+        {
+            v.push_back(ans);
+            return;
+        }
+        return;
+    }
+
+    if (sum == target)
+    {
+        v.push_back(ans);
+        return;
+    }
+
+    if (sum > target)
+    {
+        return;
+    }
+
+    // not include the element
+
+    helper(ar, si + 1, ei, ans, target, sum, v);
+
+    // include
+
+    ans.push_back(ar[si]);
+
+    helper(ar, si, ei, ans, target, sum + ar[si], v);
+    ans.pop_back();
+}
+
+vector<vector<int>> combinationSum(vector<int> &candidates, int target)
+{
+    vector<int> ans;
+    vector<vector<int>> v;
+    helper(candidates, 0, candidates.size() - 1, ans, target, 0, v);
+    return v;
+}
+};
