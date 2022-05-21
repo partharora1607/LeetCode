acffff59d729c080a180dedc064a5b81392614b0
Parth Arora <105902832+NinjaCoder3@users.noreply.github.com>
2022-05-21 19:54:53 +0530

    Time: 138 ms (56.22%), Space: 67.7 MB (91.24%) - LeetHub

---
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+int maxSubArray(vector<int> &ar)
+{
+    int ans = INT_MIN;
+    int sum = 0;
+    for (int i = 0; i < ar.size(); i++)
+    {
+        sum += ar[i];
+        ans = max(ans, sum);
+        if (sum >= 0)
+        {
+            ans = max(ans, sum);
+        }
+        else
+        {
+            sum = 0;
+        }
+    }
+    return ans;
+}
+
+};
