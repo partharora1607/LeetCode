75963c7058db7b84df10afea9305437d38c0db66
Parth Arora <105902832+NinjaCoder3@users.noreply.github.com>
2022-05-22 21:31:38 +0530

    Time: 3 ms (68.11%), Space: 7.2 MB (53.31%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,23 +1,33 @@
 class Solution {
 public:
-    void helper(vector<int> &ar, int si, int ei, vector<int> &ans, vector<vector<int>> &v)
+vector<vector<int>> helper_2(vector<int> &ar, int si, int ei)
 {
     if (si > ei)
     {
-        v.push_back(ans);
-        return;
+        return {{}};
     }
-    helper(ar, si + 1, ei, ans, v);
-    ans.push_back(ar[si]);
-    helper(ar, si + 1, ei, ans, v);
-    ans.pop_back();
+
+    vector<vector<int>> v = helper_2(ar, si + 1, ei);
+
+    int n = v.size();
+
+    for (int i = 0; i < n; i++)
+    {
+        vector<int> ans;
+        ans.push_back(ar[si]);
+        for (int j = 0; j < v[i].size(); j++)
+        {
+            ans.push_back(v[i][j]);
+        }
+        v.push_back(ans);
+        ans.clear();
+    }
+
+    return v;
 }
 
 vector<vector<int>> subsets(vector<int> &ar)
 {
-    vector<int> ans;
-    vector<vector<int>> v;
-    helper(ar, 0, ar.size() - 1, ans, v);
-    return v;
+    return helper_2(ar, 0, ar.size() - 1);
 }
 };
