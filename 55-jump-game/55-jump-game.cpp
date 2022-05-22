dd75a9b90f109cd714ef429c22c26b5ea6ad300d
Parth Arora <105902832+NinjaCoder3@users.noreply.github.com>
2022-05-22 11:40:14 +0530

    Time: 126 ms (28.83%), Space: 48.3 MB (77.77%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -1,33 +1,15 @@
 class Solution {
 public:
-bool canJump(vector<int> &ar)
+    bool canJump(vector<int> &ar)
 {
-    int n = ar.size();
-    if (n <= 1)
+    int goal = ar.size() - 1;
+    for (int i = goal - 1; i >= 0; i--)
     {
-        return true;
-    }
-    vector<int> ans(n + 1);
-    ans[0] = 1;
-    ans[1] = 1;
-    bool flag = false;
-    for (int i = 2; i <= n; i++)
-    {
-        flag = false;
-        for (int j = 1; j <= ar[n - i]; j++)
+        if (goal - i <= ar[i])
         {
-            if (ans[i - j] == 1)
-            {
-                ans[i] = 1;
-                flag = true;
-                break;
-            }
-        }
-        if (flag == false)
-        {
-            ans[i] = 0;
+            goal = i;
         }
     }
-    return ans[n];
+    return goal == 0;
 }
 };
