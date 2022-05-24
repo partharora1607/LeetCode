8754a0ae1cba29d86204fa6e4a8995bfffc6aab9
Parth Arora <105902832+NinjaCoder3@users.noreply.github.com>
2022-05-24 12:46:08 +0530

    Time: 7 ms (92.64%), Space: 6.4 MB (97.82%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -1,39 +1,24 @@
 class Solution {
 public:
-    int minDistance(string s, string t)
-{
-    int m = s.length();
-    int n = t.length();
-    int **ar = new int *[m + 1];
-    for (int i = 0; i <= m; i++)
-    {
-        ar[i] = new int[n + 1];
-    }
-    for (int i = 0; i <= m; i++)
-    {
-        ar[i][0] = i;
-    }
-    for (int j = 0; j <= n; j++)
-    {
-        ar[0][j] = j;
-    }
-    for (int i = 1; i <= m; i++)
-    {
-        for (int j = 1; j <= n; j++)
-        {
-            if (s[m - i] == t[n - j])
-            {
-                ar[i][j] = ar[i - 1][j - 1];
-            }
-            else
-            {
-                int op1 = ar[i - 1][j] + 1;
-                int op2 = ar[i - 1][j - 1] + 1;
-                int op3 = ar[i][j - 1] + 1;
-                ar[i][j] = min(op1, min(op2, op3));
+    int minDistance(string word1, string word2) {
+        int m = word1.size(), n = word2.size(), pre;
+        vector<int> cur(n + 1, 0);
+        for (int j = 1; j <= n; j++) {
+            cur[j] = j;
+        }
+        for (int i = 1; i <= m; i++) {
+            pre = cur[0];
+            cur[0] = i;
+            for (int j = 1; j <= n; j++) {
+                int temp = cur[j];
+                if (word1[i - 1] == word2[j - 1]) {
+                    cur[j] = pre;
+                } else {
+                    cur[j] = min(pre, min(cur[j - 1], cur[j])) + 1;
+                }
+                pre = temp;
             }
         }
+        return cur[n];
     }
-    return ar[m][n];
-}
 };
