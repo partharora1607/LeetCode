96119a580a7f832fa6817f98dd8b9f86d29c88d3
Parth Arora <105902832+NinjaCoder3@users.noreply.github.com>
2022-05-24 09:57:44 +0530

    Time: 0 ms (100.00%), Space: 9.3 MB (31.44%) - LeetHub

---
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    void merge(vector<int> &ar1, int m, vector<int> &ar2, int n)
+{
+    int i = 0;
+    int j = 0;
+    vector<int> v;
+    while (i < m && j < n)
+    {
+        if (ar1[i] < ar2[j])
+        {
+            v.push_back(ar1[i]);
+            i++;
+        }
+        else
+        {
+            v.push_back(ar2[j]);
+            j++;
+        }
+    }
+    while (i < m)
+    {
+        v.push_back(ar1[i++]);
+    }
+    while (j < n)
+    {
+        v.push_back(ar2[j++]);
+    }
+    for (int i = 0; i < v.size(); i++)
+    {
+        ar1[i] = v[i];
+    }
+}
+};
