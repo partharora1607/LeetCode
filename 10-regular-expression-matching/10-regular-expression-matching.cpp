06c23498f9867fc41e96110d41e4ef14291293db
Parth Arora <105902832+NinjaCoder3@users.noreply.github.com>
2022-05-27 18:21:35 +0530

    Time: 19 ms (37.30%), Space: 11.3 MB (23.79%) - LeetHub

--- 10-regular-expression-matching/10-regular-expression-matching.cpp
+++ 10-regular-expression-matching/10-regular-expression-matching.cpp
@@ -1,27 +1,51 @@
 class Solution {
 public:
-   bool isMatch(string s, string p)
+bool isMatch(string s, string p, int **ar, int m, int n)
 {
-    if (p.length() == 0)
+    if (n == 0)
     {
-        return s.length() == 0;
+        return m == 0;
     }
-    if (p.length() > 1 && p[1] == '*')
+
+    if (ar[m][n] != -1)
     {
-        if (isMatch(s, p.substr(2)))
+        return ar[m][n];
+    }
+
+    if (n > 1 && p[1] == '*')
+    {
+        if (isMatch(s, p.substr(2), ar, m, n - 2))
         {
             return true;
         }
-        if (s[0] == p[0] || p[0] == '.' && s.length() > 0)
+        if (s[0] == p[0] || p[0] == '.' && m > 0)
         {
-            return isMatch(s.substr(1), p);
+            ar[m][n] = isMatch(s.substr(1), p, ar, m - 1, n);
+            return ar[m][n];
         }
         return false;
     }
-    if (s[0] == p[0] || p[0] == '.' && s.length() > 0)
+    if (s[0] == p[0] || p[0] == '.' && m > 0)
     {
-        return isMatch(s.substr(1), p.substr(1));
+        ar[m][n] = isMatch(s.substr(1), p.substr(1), ar, m - 1, n - 1);
+        return ar[m][n];
     }
     return false;
+}
+
+bool isMatch(string s, string p)
+{
+    int m = s.length();
+    int n = p.length();
+    int **ar = new int *[m + 1];
+    for (int i = 0; i <= m; i++)
+    {
+        ar[i] = new int[n + 1];
+        for (int j = 0; j <= n; j++)
+        {
+            ar[i][j] = -1;
+        }
+    }
+    return isMatch(s, p, ar, m, n);
 }
 };
