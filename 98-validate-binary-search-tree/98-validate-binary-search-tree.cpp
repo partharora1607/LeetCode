8994b765738825139f7d04d57257b38cbf1376bd
Parth Arora <105902832+NinjaCoder3@users.noreply.github.com>
2022-06-01 17:57:57 +0530

    Time: 17 ms (51.12%), Space: 21.8 MB (33.07%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
@@ -1,29 +1,23 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-   bool helper(TreeNode *root, long long mini, long long maxi)
-{
-    if(root == NULL){
+    vector<int> v;
+    void inorder(TreeNode* root){
+        if(root==NULL){
+            return;
+        }
+        
+        inorder(root->left);
+        v.push_back(root->val);
+        inorder(root->right);
+        
+    }
+    bool isValidBST(TreeNode* root) {
+        inorder(root);
+        for(int i=0;i<v.size()-1;i++){
+            if(v[i]>=v[i+1]){
+                return false;
+            }
+        }
         return true;
     }
-    if(root->val >= maxi || root->val <= mini){
-        return false;
-    }
-    return helper(root->left , mini,root->val) && helper(root->right , root->val , maxi);
-}
-
-bool isValidBST(TreeNode *root)
-{
-    return helper(root, -2147483649, 2147483649);
-}
 };
