a8fd6284d4176b02ab595cc32a244c72f0206bd3
Parth Arora <105902832+NinjaCoder3@users.noreply.github.com>
2022-06-03 19:45:38 +0530

    Time: 0 ms (100.00%), Space: 13.6 MB (11.50%) - LeetHub

--- 404-sum-of-left-leaves/404-sum-of-left-leaves.cpp
+++ 404-sum-of-left-leaves/404-sum-of-left-leaves.cpp
@@ -12,12 +12,28 @@
 class Solution {
 public:
 
+
+int sum = 0;
+
+void helper(TreeNode *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    if (root->left != NULL && root->left->left == NULL && root->left->right == NULL)
+    {
+        sum += root->left->val;
+    }
+
+    helper(root->left);
+    helper(root->right);
+}
+
 int sumOfLeftLeaves(TreeNode *root)
 {
-    if (!root)
-        return 0;
-    if (root->left && !root->left->left && !root->left->right)
-        return root->left->val + sumOfLeftLeaves(root->right);
-    return sumOfLeftLeaves(root->left) + sumOfLeftLeaves(root->right);
+    helper(root);
+    return sum;
 }
 };
