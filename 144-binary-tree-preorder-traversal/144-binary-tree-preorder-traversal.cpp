6edcdd6e90638f4ffc18390e4c2c6c92985e6dfb
Parth Arora <105902832+NinjaCoder3@users.noreply.github.com>
2022-06-03 11:23:37 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (78.04%) - LeetHub

--- 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.cpp
@@ -12,12 +12,9 @@
 class Solution {
 public:
     vector<int> res;
-
 void helper(TreeNode *root)
 {
-    if(root == NULL){
-        return;
-    }
+    if(root == NULL) return;
     res.push_back(root->val);
     helper(root->left);
     helper(root->right);
