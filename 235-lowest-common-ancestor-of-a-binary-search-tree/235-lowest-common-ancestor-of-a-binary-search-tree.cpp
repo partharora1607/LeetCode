23cd92598ccded34fdefccd79aa91229bf5633ee
Parth Arora <105902832+NinjaCoder3@users.noreply.github.com>
2022-06-03 17:16:00 +0530

    Time: 25 ms (97.23%), Space: 23.4 MB (13.44%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -12,35 +12,16 @@ class Solution {
 public:
 TreeNode *helper(TreeNode *root, TreeNode *p, TreeNode *q)
 {
-    if (root == NULL)
-    {
-        return NULL;
-    }
-    if (root->val == p->val || root->val == q->val)
-    {
-        return root;
-    }
-    if (root->val < p->val)
-    {
-        return helper(root->right, p, q);
-    }
-    if (root->val > q->val)
-    {
-        return helper(root->left, p, q);
-    }
-    if (root->val > p->val && root->val < q->val)
-    {
-        return root;
-    }
+    if (root == NULL) return NULL;
+    if (root->val == p->val || root->val == q->val) return root;
+    if (root->val < p->val)return helper(root->right, p, q);
+    if (root->val > q->val) return helper(root->left, p, q);
+    if (root->val > p->val && root->val < q->val) return root;
     return NULL;
 }
 
 TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q)
 {
-    if (p->val > q->val)
-    {
-        return helper(root, q, p);
-    }
-    return helper(root, p, q);
+   return p->val > q->val ? helper(root, q, p) : helper(root, p, q);
 }
 };
