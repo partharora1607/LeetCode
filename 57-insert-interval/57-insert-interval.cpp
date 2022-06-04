c77dcc9afd7e9369a9a43dd15bd8b2ee2a6ce167
Parth Arora <105902832+NinjaCoder3@users.noreply.github.com>
2022-06-04 14:39:31 +0530

    Time: 15 ms (87.39%), Space: 16.8 MB (99.39%) - LeetHub

--- 57-insert-interval/57-insert-interval.cpp
+++ 57-insert-interval/57-insert-interval.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
- vector<vector<int>> insert(vector<vector<int>> &v, vector<int> &newInterval)
+vector<vector<int>> insert(vector<vector<int>> &v, vector<int> &newInterval)
 {
     vector<vector<int>> res;
-    int index = 0;
-    while (index < v.size() && v[index][1] < newInterval[0])
+    int i = 0;
+    while (i < v.size() && newInterval[0] > v[i][1])
     {
-        res.push_back(v[index++]);
+        res.push_back(v[i++]);
     }
-    while (index < v.size() && v[index][0] <= newInterval[1])
+    while (i < v.size() && newInterval[1] >= v[i][0])
     {
-        newInterval[0] = min(newInterval[0], v[index][0]);
-        newInterval[1] = max(newInterval[1], v[index][1]);
-        index++;
+        newInterval[0] = min(newInterval[0], v[i][0]);
+        newInterval[1] = max(newInterval[1], v[i][1]);
+        i++;
     }
     res.push_back(newInterval);
-    while (index < v.size())
+    while (i < v.size())
     {
-        res.push_back(v[index++]);
+        res.push_back(v[i++]);
     }
     return res;
 }
