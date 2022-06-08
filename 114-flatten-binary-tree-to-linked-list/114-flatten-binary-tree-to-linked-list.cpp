48f4be293f3843002ab3db62b943f951949e161a
Parth Arora <105902832+NinjaCoder3@users.noreply.github.com>
2022-06-08 12:24:51 +0530

    Time: 23 ms (5.83%), Space: 17.9 MB (7.40%) - LeetHub

---
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
@@ -0,0 +1,55 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    // HEAD, TAIL
+pair<TreeNode *, TreeNode *> *helper(TreeNode *root)
+{
+    if (root == NULL)
+    {
+        pair<TreeNode *, TreeNode *> *p1 = new pair<TreeNode *, TreeNode *>();
+        p1->first = NULL;
+        p1->second = NULL;
+        return p1;
+    }
+
+    if (root->left == NULL && root->right == NULL)
+    {
+        pair<TreeNode *, TreeNode *> *p1 = new pair<TreeNode *, TreeNode *>();
+        p1->first = root;
+        p1->second = root;
+        return p1;
+    }
+
+    pair<TreeNode *, TreeNode *> *leftans = helper(root->left);
+    pair<TreeNode *, TreeNode *> *rightans = helper(root->right);
+    root->left = NULL;
+    if (leftans->first != NULL && leftans->second != NULL)
+    {
+        root->right = leftans->first;
+        leftans->second->right = rightans->first;
+    }
+    else
+    {
+        root->right = rightans->first;
+    }
+    pair<TreeNode *, TreeNode *> *ans = new pair<TreeNode *, TreeNode *>();
+    ans->first = root;
+    ans->second = rightans->second != NULL ? rightans->second : leftans->second;
+    return ans;
+}
+
+void flatten(TreeNode *root)
+{
+    helper(root);
+}
+};
