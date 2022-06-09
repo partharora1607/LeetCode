e309cbff03f3436b3b64210eb744eb165eac6115
Parth Arora <105902832+NinjaCoder3@users.noreply.github.com>
2022-06-09 19:34:21 +0530

    Time: 20 ms (71.47%), Space: 15.6 MB (67.86%) - LeetHub

---
+++ 95-unique-binary-search-trees-ii/95-unique-binary-search-trees-ii.cpp
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<TreeNode *> helper(int si, int ei)
+{
+    vector<TreeNode *> res;
+    if (si > ei)
+    {
+        res.push_back(NULL);
+        return res;
+    }
+
+    if (si == ei)
+    {
+        TreeNode *root = new TreeNode(si);
+        res.push_back(root);
+        return res;
+    }
+
+    for (int i = si; i <= ei; i++)
+    {
+        vector<TreeNode *> leftans = helper(si, i - 1);
+        vector<TreeNode *> rigthans = helper(i + 1, ei);
+
+        for (int j = 0; j < leftans.size(); j++)
+        {
+            for (int k = 0; k < rigthans.size(); k++)
+            {
+                TreeNode *root = new TreeNode(i);
+                root->left = leftans[j];
+                root->right = rigthans[k];
+                res.push_back(root);
+            }
+        }
+    }
+    return res;
+}
+
+vector<TreeNode *> generateTrees(int n)
+{
+    return helper(1, n);
+}
+};
