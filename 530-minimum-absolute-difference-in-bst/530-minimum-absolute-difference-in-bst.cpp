2ea5382911ce6230984b2afc5ae85b6ae705731a
Parth Arora <105902832+NinjaCoder3@users.noreply.github.com>
2022-06-09 08:59:54 +0530

    Time: 19 ms (82.59%), Space: 31.3 MB (5.88%) - LeetHub

---
+++ 530-minimum-absolute-difference-in-bst/530-minimum-absolute-difference-in-bst.cpp
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    pair<int, pair<int, int>> *helper(TreeNode *root)
+{
+    if (root == NULL)
+    {
+        pair<int, pair<int, int>> *p1 = new pair<int, pair<int, int>>();
+        p1->first = INT_MAX;
+        p1->second.first = INT_MAX;
+        p1->second.second = INT_MIN;
+        return p1;
+    }
+
+    pair<int, pair<int, int>> *leftans = helper(root->left);
+    pair<int, pair<int, int>> *rightans = helper(root->right);
+
+    pair<int, pair<int, int>> *ans = new pair<int, pair<int, int>>();
+
+    ans->second.first = min(root->val, min(leftans->second.first, rightans->second.first));
+    ans->second.second = max(root->val, max(leftans->second.second, rightans->second.second));
+    int ok = INT_MAX;
+    if (root->left != NULL)
+    {
+        ok = min(ok, abs(root->val - leftans->second.second));
+    }
+    if (root->right != NULL)
+    {
+        ok = min(ok, abs(root->val - rightans->second.first));
+    }
+    int op1 = ok;
+    int op2 = leftans->first;
+    int op3 = rightans->first;
+    ans->first = min(op1, min(op2, op3));
+    return ans;
+}
+
+int getMinimumDifference(TreeNode *root)
+{
+    return helper(root)->first;
+}
+};
