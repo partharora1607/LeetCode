c2384da0044ad690e76f7870d42af0ac76cad1ef
Parth Arora <105902832+NinjaCoder3@users.noreply.github.com>
2022-06-09 17:48:03 +0530

    Time: 21 ms (33.48%), Space: 21.4 MB (10.79%) - LeetHub

--- 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.cpp
+++ 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.cpp
@@ -12,7 +12,6 @@
 class Solution {
 public:
 int res = 0;
-
 // no of nodes , sum
 pair<int, int> *helpeR(TreeNode *root)
 {
@@ -21,21 +20,16 @@ pair<int, int> *helpeR(TreeNode *root)
         pair<int, int> *p1 = new pair<int, int>(0, 0);
         return p1;
     }
-
     pair<int, int> *leftans = helpeR(root->left);
     pair<int, int> *rightans = helpeR(root->right);
-
     pair<int, int> *ans = new pair<int, int>();
-
     ans->first = leftans->first + rightans->first + 1;
     ans->second = leftans->second + rightans->second + root->val;
-
     // check
     if ((ans->second / ans->first) == root->val)
     {
         res++;
     }
-
     return ans;
 }
 
