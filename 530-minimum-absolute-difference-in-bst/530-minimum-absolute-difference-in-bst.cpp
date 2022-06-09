5c91df8604ee2295a4c045406234f6e53e6b63da
Parth Arora <105902832+NinjaCoder3@users.noreply.github.com>
2022-06-09 09:03:17 +0530

    Time: 44 ms (9.76%), Space: 31.2 MB (5.88%) - LeetHub

--- 530-minimum-absolute-difference-in-bst/530-minimum-absolute-difference-in-bst.cpp
+++ 530-minimum-absolute-difference-in-bst/530-minimum-absolute-difference-in-bst.cpp
@@ -21,27 +21,17 @@ public:
         p1->second.second = INT_MIN;
         return p1;
     }
-
     pair<int, pair<int, int>> *leftans = helper(root->left);
     pair<int, pair<int, int>> *rightans = helper(root->right);
-
     pair<int, pair<int, int>> *ans = new pair<int, pair<int, int>>();
-
     ans->second.first = min(root->val, min(leftans->second.first, rightans->second.first));
     ans->second.second = max(root->val, max(leftans->second.second, rightans->second.second));
     int ok = INT_MAX;
-    if (root->left != NULL)
-    {
-        ok = min(ok, abs(root->val - leftans->second.second));
-    }
-    if (root->right != NULL)
-    {
-        ok = min(ok, abs(root->val - rightans->second.first));
-    }
-    int op1 = ok;
+    if (root->left) ok = min(ok, abs(root->val - leftans->second.second));
+    if (root->right) ok = min(ok, abs(root->val - rightans->second.first));
     int op2 = leftans->first;
     int op3 = rightans->first;
-    ans->first = min(op1, min(op2, op3));
+    ans->first = min(ok, min(op2, op3));
     return ans;
 }
 
