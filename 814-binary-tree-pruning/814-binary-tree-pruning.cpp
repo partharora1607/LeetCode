353c675c4d61cd3bfd9d523b346d1b90935d0f0f
Parth Arora <105902832+NinjaCoder3@users.noreply.github.com>
2022-06-14 17:59:31 +0530

    Time: 7 ms (22.99%), Space: 8.8 MB (65.01%) - LeetHub

--- 814-binary-tree-pruning/814-binary-tree-pruning.cpp
+++ 814-binary-tree-pruning/814-binary-tree-pruning.cpp
@@ -11,44 +11,20 @@
  */
 class Solution {
 public:
-// node , hasone(flag)
-
-pair<TreeNode *, bool> *helper(TreeNode *root)
-{
-    if (root == NULL)
-    {
-        pair<TreeNode *, bool> *p1 = new pair<TreeNode *, bool>(NULL, false);
-        return p1;
+    TreeNode* pruneTree(TreeNode* root) {
+        if(root == NULL){
+            return NULL;
+        }
+        
+        root->left = pruneTree(root->left);
+        root->right = pruneTree(root->right);
+        
+        if(root->left == NULL && root->right == NULL){
+            if(root->val == 0){
+                return NULL;
+            }
+        }
+        
+        return root;
     }
-
-    pair<TreeNode *, bool> *leftans = helper(root->left);
-    pair<TreeNode *, bool> *rightans = helper(root->right);
-    
-    root->left = leftans->first;
-    root->right = rightans->first;
- 
-    if (root->val == 1)
-    {
-        pair<TreeNode *, bool> *p1 = new pair<TreeNode *, bool>(root, true);
-        return p1;
-    }
-
-    pair<TreeNode *, bool> *ans = new pair<TreeNode *, bool>();
-    if (leftans->second == false && rightans->second == false && root->val == 0)
-    {
-        ans->first = NULL;
-        ans->second = false;
-    }
-    else
-    {
-        ans->first = root;
-        ans->second = true;
-    }
-    return ans;
-}
-
-TreeNode *pruneTree(TreeNode *root)
-{
-    return helper(root)->first;
-}
 };
