ac7d43558a5b205b64f99f0e4288844cae11bbcb
Parth Arora <105902832+NinjaCoder3@users.noreply.github.com>
2022-06-14 17:36:33 +0530

    Time: 0 ms (100.00%), Space: 10.7 MB (5.69%) - LeetHub

---
+++ 814-binary-tree-pruning/814-binary-tree-pruning.cpp
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    pair<TreeNode *, bool> *helper(TreeNode *root)
+{
+    if (root == NULL)
+    {
+        pair<TreeNode *, bool> *p1 = new pair<TreeNode *, bool>(NULL, false);
+        return p1;
+    }
+
+    pair<TreeNode *, bool> *leftans = helper(root->left);
+    pair<TreeNode *, bool> *rightans = helper(root->right);
+
+    root->left = leftans->first;
+    root->right = rightans->first;
+    
+    if (root->val == 1)
+    {
+        pair<TreeNode *, bool> *p1 = new pair<TreeNode *, bool>(root, true);
+        return p1;
+    }
+
+    pair<TreeNode *, bool> *ans = new pair<TreeNode *, bool>();
+
+    if (leftans->second == false && rightans->second == false && root->val == 0)
+    {
+        ans->first = NULL;
+        ans->second = false;
+    }
+    else
+    {
+        ans->first = root;
+        ans->second = true;
+    }
+    return ans;
+}
+
+TreeNode *pruneTree(TreeNode *root)
+{
+    return helper(root)->first;
+}
+};
