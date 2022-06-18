3e692d17d445deb9d48e6a786843bec0467e8c5c
Parth Arora <105902832+NinjaCoder3@users.noreply.github.com>
2022-06-18 11:11:45 +0530

    Time: 3 ms (64.30%), Space: 8.4 MB (31.21%) - LeetHub

--- 655-print-binary-tree/655-print-binary-tree.cpp
+++ 655-print-binary-tree/655-print-binary-tree.cpp
@@ -11,33 +11,30 @@
  */
 class Solution {
 public:
-    // just trying to create height of tree  function
-    int height(TreeNode *root)
-    {
-        if(!root)
-            return 0;
-        int left=height(root->left);
-        int right=height(root->right);
-        return max(left,right)+1;
-    }
-     void chalomatrixbanao(TreeNode* root, int l, int r, int h, vector<vector<string>>& ans) {
-        if (root == NULL) return;
-        if (l > r) return;
-        int mid = (l + r) / 2;
-        ans[h][mid] = to_string(root->val);
-        chalomatrixbanao(root->left, l, mid-1, h+1, ans);
-        chalomatrixbanao(root->right, mid+1, r, h+1, ans);
-    }
-    vector<vector<string>> printTree(TreeNode* root) {
-        // actually it is a simpke question of constructing tree from matrix 
-      
-        int h=height(root);
-        int c=pow(2,h)-1;
-        vector<vector<string>>res(h,vector<string>(c,""));
-        chalomatrixbanao(root, 0, c-1, 0, res);
-        return res;
-        
-        
-        
-    }
+    int getheight(TreeNode *root)
+{
+    if (!root)
+        return 0;
+    return max(getheight(root->left), getheight(root->right)) + 1;
+}
+
+void chalochalo(TreeNode *root, int si, int ei, int h, vector<vector<string>> &v)
+{
+    if(root == NULL) return;
+    if(si > ei) return;
+    int mid = (si + ei)/2;
+    v[h][mid] = to_string(root->val);
+    chalochalo(root->left , si , mid - 1 , h + 1 , v);
+    chalochalo(root->right, mid + 1 , ei , h + 1 , v);
+}
+
+vector<vector<string>> printTree(TreeNode *root)
+{
+    int h = getheight(root);
+    int r = h;
+    int c = pow(2, h) - 1;
+    vector<vector<string>> v(r, vector<string>(c, ""));
+    chalochalo(root, 0, c - 1, 0, v);
+    return v;
+}
 };
