62e4fee7c9c58a659c79b96160a80b1a919c56bf
Parth Arora <105902832+NinjaCoder3@users.noreply.github.com>
2022-06-25 13:34:59 +0530

    Time: 6 ms (41.85%), Space: 6 MB (18.14%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,9 +1,15 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n == 0 ||n == 1){
+        if(n == 0 || n == 1){
             return n;
         }
-        return fib(n - 1) + fib(n - 2);
+        vector<int> ar(n + 1);
+        ar[0] = 0;
+        ar[1] = 1;
+        for(int i = 2;i <= n ; i++){
+            ar[i] = ar[i - 1] + ar[i - 2];
+        }
+        return ar[n];
     }
 };
