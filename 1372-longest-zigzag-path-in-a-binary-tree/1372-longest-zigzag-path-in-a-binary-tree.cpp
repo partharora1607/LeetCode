3e26fd046ae9ba2cff1edd391253fdf033d83d60
Parth Arora <105902832+NinjaCoder3@users.noreply.github.com>
2022-06-26 21:19:48 +0530

    Time: 389 ms (18.34%), Space: 126.2 MB (17.85%) - LeetHub

---
+++ 1372-longest-zigzag-path-in-a-binary-tree/1372-longest-zigzag-path-in-a-binary-tree.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int res = 0;
+
+// leftans , rightans
+pair<int, int> *helper(TreeNode *root)
+{
+    if (root == NULL)
+    {
+        pair<int, int> *p1 = new pair<int, int>(-1, -1);
+        return p1;
+    }
+
+    if (root->left == NULL && root->right == NULL)
+    {
+        pair<int, int> *p1 = new pair<int, int>(0, 0);
+        return p1;
+    }
+
+    pair<int, int> *leftans = helper(root->left);
+    pair<int, int> *rightans = helper(root->right);
+
+    pair<int, int> *ans = new pair<int, int>();
+
+    // ans -> leftans
+    ans->first = leftans->second + 1;
+    ans->second = rightans->first + 1;
+
+    res = max(res, max(ans->first, ans->second));
+    return ans;
+}
+
+int longestZigZag(TreeNode *root)
+{
+    helper(root);
+    return res;
+}
+};
