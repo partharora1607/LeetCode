7a08db1ddb2413db17a253547e8fdf2ad897d95d
Parth Arora <105902832+NinjaCoder3@users.noreply.github.com>
2022-07-11 16:38:33 +0530

    Time: 12 ms (30.37%), Space: 16.8 MB (7.36%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.cpp
+++ 101-symmetric-tree/101-symmetric-tree.cpp
@@ -11,21 +11,52 @@
  */
 class Solution {
 public:
-    bool helper(TreeNode *root1, TreeNode *root2)
-{
-    if (!root1 && !root2)
-        return true;
-    if (!root1 || !root2)
+    // Method - 2 (Trying Iteratively)
+
+bool check(TreeNode *root1 , TreeNode *root2){
+    if(!root1 && !root2) return true;
+    if(!root1 || !root2) return false;
+    if(root1->val != root2->val){
         return false;
-    return helper(root1->left , root2->right) && helper(root1->right , root2->left) && (root1->val ==  root2->val);
+    }
+    return true;
+}
+
+bool helper(TreeNode *root1 , TreeNode *root2){
+
+    if(!check(root1 , root2)) return false;
+    queue<TreeNode *> q1;
+    q1.push(root1);
+    queue<TreeNode *> q2;
+    q2.push(root2);
+    while(!q1.empty() && !q2.empty()){
+        TreeNode *front1 = q1.front();
+        TreeNode *front2 = q2.front();
+        q1.pop();
+        q2.pop();
+
+        if(!check(front1 , front2)) return false;
+        if(front1)
+        {
+           q1.push(front1->left);
+           q1.push(front1->right);
+        }
+        if(front2){
+            q2.push(front2->right);
+            q2.push(front2->left);
+        }
+    }
+    return true;
 }
 
 bool isSymmetric(TreeNode *root)
 {
-    if (!root)
+    if(root == NULL){
         return true;
-    if (!root->left && !root->right)
+    }
+    if(root->left == NULL && root->right == NULL){
         return true;
-    return helper(root->left, root->right);
+    }
+    return helper(root->left , root->right);
 }
 };
