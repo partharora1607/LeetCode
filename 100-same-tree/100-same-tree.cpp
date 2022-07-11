e66e57231fe8408dd994097d2f378fb74923b9d2
Parth Arora <105902832+NinjaCoder3@users.noreply.github.com>
2022-07-11 15:52:01 +0530

    Time: 4 ms (44.07%), Space: 10.1 MB (44.32%) - LeetHub

--- 100-same-tree/100-same-tree.cpp
+++ 100-same-tree/100-same-tree.cpp
@@ -15,8 +15,7 @@ public:
 {
     if(!p && !q) return true;
     if(!p || !q) return false;
-    if(p->val != q->val) return false;
-    return isSameTree(p->left , q->left) && isSameTree(p->right , q->right);
+    return isSameTree(p->left , q->left) && isSameTree(p->right , q->right) && (p->val == q->val);
 }
 
 };
