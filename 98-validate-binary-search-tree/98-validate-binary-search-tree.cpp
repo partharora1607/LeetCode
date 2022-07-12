19a3182b9882c266272ecae9f4cbbb38926acbb2
Parth Arora <105902832+NinjaCoder3@users.noreply.github.com>
2022-07-12 15:24:20 +0530

    Time: 22 ms (38.63%), Space: 21.8 MB (30.43%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
@@ -1,23 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    vector<int> v;
-    void inorder(TreeNode* root){
-        if(root==NULL){
-            return;
+vector<int> res;
+
+void inorder(TreeNode *root){
+    if(!root) return;
+    inorder(root->left);
+    res.push_back(root->val);
+    inorder(root->right);
+}
+
+bool isValidBST(TreeNode *root)
+{
+     inorder(root);
+    for (int i = 1; i < res.size(); i++)
+    {   
+        if(res[i] <= res[i - 1]){
+            return false;
         }
-        
-        inorder(root->left);
-        v.push_back(root->val);
-        inorder(root->right);
-        
     }
-    bool isValidBST(TreeNode* root) {
-        inorder(root);
-        for(int i=0;i<v.size()-1;i++){
-            if(v[i]>=v[i+1]){
-                return false;
-            }
-        }
-        return true;
-    }
+    return true;    
+}
 };
