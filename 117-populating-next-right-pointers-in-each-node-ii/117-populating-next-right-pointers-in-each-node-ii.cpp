556417ca1e88b089da2d4ffb19563e1cf40c8cc3
Parth Arora <105902832+NinjaCoder3@users.noreply.github.com>
2022-07-13 11:27:23 +0530

    Time: 24 ms (29.79%), Space: 17.4 MB (67.35%) - LeetHub

--- 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.cpp
+++ 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.cpp
@@ -18,48 +18,35 @@ public:
 
 class Solution {
 public:
-    vector<Node *> res;
-
-Node *connect(Node *root)
+    Node *connect(Node *root)
 {
-    if (root == NULL)
-    {
+    // edge
+
+    if (!root)
         return root;
-    }
+    if (!root->left && !root->right)
+        return root;
+
     queue<Node *> q1;
     q1.push(root);
-    q1.push(NULL);
+
     while (!q1.empty())
     {
-        Node *front = q1.front();
-        q1.pop();
 
-        if (front != NULL)
+        int n = q1.size();
+        for (int i = 0; i < n; i++)
         {
-            res.push_back(front);
-            if (front->left)
-            {
-                q1.push(front->left);
+            Node *front = q1.front();
+            q1.pop();
+
+            if(!q1.empty() && i < n - 1){
+                front->next = q1.front();
             }
-            if (front->right)
-            {
-                q1.push(front->right);
-            }
-        }
-        else
-        {
-            for (int i = 0; i < res.size() - 1; i++)
-            {
-                res[i]->next = res[i + 1];
-            }
-            res.clear();
-            if (!q1.empty())
-            {
-                q1.push(NULL);
-            }
+
+            if(front->left) q1.push(front->left);
+            if(front->right) q1.push(front->right);
         }
     }
     return root;
 }
-
 };
