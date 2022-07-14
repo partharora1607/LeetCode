ae21e8d1c9be00cdcdc6476def7616bfe502fc13
Parth Arora <105902832+NinjaCoder3@users.noreply.github.com>
2022-07-14 20:05:06 +0530

    Time: 61 ms (19.70%), Space: 16 MB (76.61%) - LeetHub

--- 437-path-sum-iii/437-path-sum-iii.cpp
+++ 437-path-sum-iii/437-path-sum-iii.cpp
@@ -11,28 +11,25 @@
  */
 class Solution {
 public:
-    
-int helper(TreeNode *root, long long int currsum, int targetsum)
+    int find_paths(TreeNode *root, int target , long long int sum = 0)
 {
     if (root == NULL)
-    {
         return 0;
+    sum += root->val;
+    int ans = 0;
+    if(sum == target){
+        ans += 1;
     }
-    currsum += root->val;
-    if (currsum == targetsum)
-    {
-        return helper(root->left, currsum, targetsum) + helper(root->right, currsum, targetsum) + 1;
-    }
-    return helper(root->left, currsum, targetsum) + helper(root->right, currsum, targetsum);
+    int leftans = find_paths(root->left , target , sum);
+    int rightans = find_paths(root->right , target , sum); 
+    return leftans + rightans + ans;
 }
 
 int pathSum(TreeNode *root, int targetSum)
 {
     if (root == NULL)
-    {
         return 0;
-    }
-    int ans1 = helper(root, 0, targetSum);
+    int ans1 = find_paths(root, targetSum);
     int leftans = pathSum(root->left, targetSum);
     int rightans = pathSum(root->right, targetSum);
     return ans1 + leftans + rightans;
