bc34c7fe0023f557b5be82f1b3eea2e91521e31f
Parth Arora <105902832+NinjaCoder3@users.noreply.github.com>
2022-07-15 13:21:08 +0530

    Time: 3 ms (95.47%), Space: 12.4 MB (84.38%) - LeetHub

--- 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.cpp
+++ 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.cpp
@@ -9,52 +9,60 @@
  */
 class Solution {
 public:
-    
+    // root to node distance
 vector<int> res;
 
-void Node_at_depth_k(TreeNode *root , int k){
-    if(root == NULL || k < 0){
+void nodes_at_depth_k(TreeNode *root, int k , int level = 0)
+{
+    if (root == NULL || k < 0)
+    {
         return;
     }
-    if(k == 0){
+    if (k == level)
+    {
         res.push_back(root->val);
         return;
     }
-    Node_at_depth_k(root->left , k - 1);
-    Node_at_depth_k(root->right , k - 1);
+    nodes_at_depth_k(root->left, k , level + 1);
+    nodes_at_depth_k(root->right, k  , level + 1);
 }
 
-// return distance , -1 -> not found
-
-int helper(TreeNode *root , TreeNode *target , int k){
-    if(root == NULL){
-        return -1; // node not found
-    }
-
-    if(root->val == target->val){
-        Node_at_depth_k(root, k);
-        return 0; // dis fron root to root == 0;
-    }
-
-    int ld = helper(root->left , target , k);
-    if(ld != -1)
+// distance from root to leaf
+int helper(TreeNode *root, TreeNode *target, int k)
+{
+    if (root == NULL)
     {
-        if(ld + 1 == k){
-            res.push_back(root->val);
-        }
-        else{
-            Node_at_depth_k(root->right , k - ld - 2);
-        }
-        return ld+1;
+        return -1;
     }
-    int rd = helper(root->right , target , k);
-    if(rd != -1){
-        if(rd + 1 == k){
+    if (root->val == target->val)
+    {
+        nodes_at_depth_k(root, k);
+        return 0;
+    }
+
+    int ld = helper(root->left, target, k);
+
+    if (ld != -1)
+    {
+        if (ld + 1 == k)
+        {
             res.push_back(root->val);
+            return ld +1 ;
         }
-        else{
-            Node_at_depth_k(root->left , k -rd - 2);
+            nodes_at_depth_k(root->right, k - ld - 2);
+        return ld + 1;
+    }
+
+    int rd = helper(root->right, target, k);
+
+    if (rd != -1)
+    {
+        if (rd + 1 == k)
+        {
+            res.push_back(root->val);
+            return rd + 1;
         }
+          nodes_at_depth_k(root->left, k - rd - 2);
         return rd + 1;
     }
     return -1;
