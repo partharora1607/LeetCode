e5ac7cbfd9291c77c05a6e1870e99ac2814bf241
Parth Arora <105902832+NinjaCoder3@users.noreply.github.com>
2022-07-15 11:57:20 +0530

    Time: 109 ms (11.43%), Space: 64.4 MB (20.97%) - LeetHub

--- 652-find-duplicate-subtrees/652-find-duplicate-subtrees.cpp
+++ 652-find-duplicate-subtrees/652-find-duplicate-subtrees.cpp
@@ -11,28 +11,31 @@
  */
 class Solution {
 public:
-    string dfs(TreeNode *root, unordered_map<string, int> &m, vector<TreeNode *> &ans)
+vector<TreeNode *> res;
+unordered_map<string, int> mymap;
+
+string dfs(TreeNode *root)
 {
     if (root == NULL)
     {
         return "NULL";
     }
-    string s = to_string(root->val) + ",";
-    string a = dfs(root->left, m, ans) + "," ;
-    string b = dfs(root->right, m, ans);
+    string s = to_string(root->val);
+    s += ',';
+    string a = dfs(root->left);
+    a += ',';
+    string b = dfs(root->right);
     s = s + a + b;
-    if (m[s]++ == 1)
-    {
-        ans.push_back(root);
+    mymap[s]++;
+    if(mymap[s] == 2){
+        res.push_back(root);
     }
-    return s;
+    return s;   
 }
 
 vector<TreeNode *> findDuplicateSubtrees(TreeNode *root)
 {
-    unordered_map<string, int> m;
-    vector<TreeNode *> ans;
-    dfs(root, m, ans);
-    return ans;
+    dfs(root);
+    return res;
 }
 };
