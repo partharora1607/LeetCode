7b1df9cb61b87d70ae22b5d5b9f51124049d9daa
Parth Arora <105902832+NinjaCoder3@users.noreply.github.com>
2022-07-19 14:40:44 +0530

    Time: 599 ms (22.23%), Space: 18.2 MB (6.23%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -1,21 +1,34 @@
 class Solution {
 public:
+int jump(vector<int> &ar, int si, int ei, vector<int> &output)
+{
+    if (si >= ei)
+    {
+        return 0;
+    }
+
+    int n = ei - si + 1;
+
+    if (output[n] != -1)
+    {
+        return output[n];
+    }
+
+    int ans = pow(10, 5);
+
+    for (int i = 1; i <= ar[si]; i++)
+    {
+        int ans1 = jump(ar, si + i, ei, output) + 1;
+        ans = min(ans1, ans);
+    }
+    output[n] = ans;
+    return ans;
+}
+
 int jump(vector<int> &ar)
 {
-    int ans = 0;
-    int l = 0, r = 0;
-    int goal = ar.size() - 1;
-    while (r < goal)
-    {
-        int farthest = 0;
-        for (int i = l; i <= r; i++)
-        {
-            farthest = max(farthest, ar[i] + i);
-        }
-        l = r + 1;
-        r = farthest;
-        ans++;
-    }
-    return ans;
+    int n = ar.size();
+    vector<int> output(n + 1, -1);
+    return jump(ar, 0, ar.size() - 1, output);
 }
 };
