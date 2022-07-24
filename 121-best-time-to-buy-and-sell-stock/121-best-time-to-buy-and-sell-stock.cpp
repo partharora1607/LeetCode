df7a7c8e37a6ea08de4d645a9847f3b2ae63edd1
Parth Arora <105902832+NinjaCoder3@users.noreply.github.com>
2022-07-24 21:49:24 +0530

    Time: 182 ms (60.62%), Space: 97.1 MB (6.64%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -3,25 +3,21 @@ public:
 int maxProfit(vector<int> &ar)
 {
     int n = ar.size();
-
     if(n <= 1){
         return 0;
     }
-
-    int L = 0;
-    int R = 1;
+    vector<int> res(n);
+    int min_so_far = ar[0];
+    res[0] = 0;
 
     int ans = 0;
 
-    while(R < n){
-        while(R < n && ar[R] > ar[L]){
-            ans = max(ans , ar[R] - ar[L]);
-            R++;
-        }
-        L = R;
-        R++;
+    for (int i = 1; i < n; i++)
+    {
+        res[i] = max(ar[i] - min_so_far , 0);
+        min_so_far = min(min_so_far , ar[i]);
+        ans = max(ans , res[i]);
     }
-
     return ans;
 }
 };
