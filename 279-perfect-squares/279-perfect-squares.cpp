4d3207b8a2a7e6a61840f36461f8885534a5ac78
Parth Arora <105902832+NinjaCoder3@users.noreply.github.com>
2022-07-30 23:13:25 +0530

    Time: 100 ms (88.51%), Space: 9.1 MB (50.59%) - LeetHub

--- 279-perfect-squares/279-perfect-squares.cpp
+++ 279-perfect-squares/279-perfect-squares.cpp
@@ -1,32 +1,29 @@
 class Solution {
 public:
-int helper(int n, vector<int> &output)
+    int numSquares(int n)
 {
-    if (n <= 3)
-    {
+    if(n <= 3){
         return n;
     }
+    vector<int> output(n + 1);
 
-    if (output[n] != -1)
-    {
-        return output[n];
+    output[0] = 0;
+    output[1] = 1;
+    output[2] = 2;
+    output[3] = 3;
+
+    for (int i = 4; i <= n; i++)
+    {   
+        int j = 1;
+        int ans = pow(10,7);
+        while(j * j <= i){
+            int smallans = output[i - (j * j)] + 1;
+            ans = min(ans , smallans);
+            j++;
+        }
+        output[i] = ans;
     }
+    return output[n];
 
-    int i = 1;
-    int ans = pow(10, 5);
-
-    while (i * i <= n)
-    {
-        int smallans = helper(n - (i * i), output) + 1;
-        ans = min(ans, smallans);
-        i++;
-    }
-    output[n] = ans;
-    return ans;
-}
-int numSquares(int n)
-{
-    vector<int> output(n + 1, -1);
-    return helper(n, output);
 }
 };
