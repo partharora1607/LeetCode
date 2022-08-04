2bb1a79665193aee1aeb978aae77beb24f8c8e49
Parth Arora <105902832+NinjaCoder3@users.noreply.github.com>
2022-08-04 18:18:54 +0530

    Time: 9 ms (9.76%), Space: 7.9 MB (9.58%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.cpp
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -1,38 +1,37 @@
 class Solution {
 public:
-bool helper(string s, string t, int m, int n, vector<vector<int>> &output)
-{
-    if (m == 0)
-    {
-        return true;
-    }
-
-    if (m > n)
-    {
-        return false;
-    }
-
-    if (output[m][n] != -1)
-    {
-        return output[m][n];
-    }
-
-    if (s[0] == t[0])
-    {
-        output[m][n] = helper(s.substr(1), t.substr(1), m - 1, n - 1, output);
-        return output[m][n];
-    }
-
-    output[m][n] = helper(s , t.substr(1) , m , n - 1 , output);
-    return output[m][n];
-
-}
-
 bool isSubsequence(string s, string t)
 {
     int m = s.length();
     int n = t.length();
     vector<vector<int>> output(m + 1, vector<int>(n + 1, -1));
-    return helper(s, t, m, n, output);
+
+    // m -> s ->i
+    // n - > r -> j
+
+    for (int j = 0; j <= n; j++)
+    {
+        output[0][j] = 1;
+    }
+
+    for (int i = 1; i <= m; i++)
+    {
+        for (int j = 0; j <= n; j++)
+        {
+            if (i > j)
+            {
+                output[i][j] = 0;
+            }
+            else if (s[m - i] == t[n - j])
+            {
+                output[i][j] = output[i - 1][j - 1];
+            }
+            else
+            {
+                output[i][j] = output[i][j - 1];
+            }
+        }
+    }
+    return output[m][n];
 }
 };
