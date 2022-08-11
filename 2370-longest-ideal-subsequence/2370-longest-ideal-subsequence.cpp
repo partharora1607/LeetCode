c9d4645773bbb5ed501038d74c2943d46a73e768
CodeChefer <105902832+CodeChefer@users.noreply.github.com>
2022-08-11 16:36:22 +0530

    Time: 97 ms (85.71%), Space: 10.2 MB (85.71%) - LeetHub

--- 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.cpp
+++ 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
-int longestIdealString(string s, int k) {
-		vector<int> dp(26, 0);
-        int maxi = 1;
-		for (int i = 0; i < s.size(); i++) {
-			int ind = s[i] - 'a';
-			int ans = 1;
-			for (int j = 0; j < 26; j++) {
-				if (abs(j - ind) <= k) {
-					ans = max(ans, dp[j] + 1);
-				}
-			}
-			dp[ind] = ans;
-            maxi = max(maxi,dp[ind]);
-		}
-    return maxi;
-	}
+   int longestIdealString(string s, int k)
+{
+    int n = s.size();
+    vector<int> dp(26, 0);
+    dp[s[n - 1] - 'a'] = 1;
+    for (int i = n - 2; i >= 0; i--)
+    {
+        int lb = max(0, (s[i] - 'a') - k);
+        int hb = min(25, (s[i] - 'a') + k);
+
+        int ans = 0;
+
+        for (int i = lb; i <= hb; i++)
+        {
+            ans = max(ans, dp[i]);
+        }
+        dp[s[i] - 'a'] = max(dp[s[i] - 'a'], ans + 1);
+    }
+    return *max_element(dp.begin() , dp.end());
+}
 };
