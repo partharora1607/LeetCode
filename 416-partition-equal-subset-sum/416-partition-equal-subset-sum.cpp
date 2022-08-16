4e83d8b54316f8e82d805ec2047b9cf0402c6e4b
CodeChefer <105902832+CodeChefer@users.noreply.github.com>
2022-08-16 23:57:44 +0530

    Time: 257 ms (63.23%), Space: 84.8 MB (16.98%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -1,48 +1,30 @@
 class Solution {
 public:
-bool helper(vector<int> &ar, int si, int ei, int target, int sum, vector<vector<int>> &output)
-{
-    if (sum == target)
-    {
-         return true;
+    
+    bool helper(vector<int> &nums , int si , int ei , int sum ,const int& target , vector<vector<int>> &output){
+        if(si > ei){
+            if(sum == target) return true;
+            return false;
+        }
+        
+        if(sum > target) return false;
+        
+        if(output[si][sum] != -1) return output[si][sum];
+        
+        return output[si][sum] = helper(nums , si + 1 , ei , sum , target , output) || helper(nums , si + 1 , ei , sum + nums[si] , target , output);
+        
     }
     
-    if (si > ei)
-    {
-        return false;
+    bool canPartition(vector<int>& nums) {
+        int n = nums.size();
+        int sum = 0;
+        for(auto i:nums){
+            sum += i;
+        }
+        if(sum % 2 != 0){
+            return false;
+        }
+        vector<vector<int>> output(n , vector<int>(sum / 2 + 1 , -1));
+        return helper(nums , 0 , n - 1 , 0 , sum / 2 , output);
     }
-
-    if (sum > target)
-    {
-        return false;
-    }
-
-    if (output[si][sum] != -1)
-    {
-        return output[si][sum];
-    }
-
-    bool op1 = helper(ar, si + 1, ei, target, sum, output);
-    bool op2 = helper(ar, si + 1, ei, target, sum + ar[si], output);
-
-    output[si][sum] = op1 || op2;
-    return op1 || op2;
-}
-
-bool canPartition(vector<int> &ar)
-{
-    int sum = 0;
-    int n = ar.size();
-    for (int i = 0; i < n; i++)
-    {
-        sum += ar[i];
-    }
-    if(sum % 2 != 0){
-        return false;
-    }
-    int target = sum / 2;
-    vector<vector<int>> output(n, vector<int>(target + 1, -1));
-    return helper(ar, 0, n - 1, target, 0, output);
-}
-
 };
