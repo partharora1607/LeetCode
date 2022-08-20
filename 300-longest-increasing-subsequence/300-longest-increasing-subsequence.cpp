e8ba95910e2474be228035d1d20b0e09c719b1d2
CodeChefer <105902832+CodeChefer@users.noreply.github.com>
2022-08-20 17:23:58 +0530

    Time: 337 ms (56.03%), Space: 10.5 MB (44.12%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
-   int lengthOfLIS(vector<int> &ar)
-{
-    int n = ar.size();
-    if(n <= 1){
-        return n;
-    }
-    vector<int> dp(n);
-    dp[0] = 1;
-    int maxi = 1;
-    for (int i = 1; i < n; i++)
-    {
-        int ans = 0;
-        for (int j = 0; j < i; j++)
-        {
-            if (ar[j] < ar[i])
-            {
-                ans = max(ans, dp[j]);
+    int lengthOfLIS(vector<int>& nums) {
+            int n = nums.size();
+            if(n <= 1){
+                return n;
             }
-        }
-        dp[i] = ans + 1;
-        maxi = max(maxi, dp[i]);
+            vector<int> dp(n);
+            dp[0] = 1;
+            int maxi = 1;
+            for(int i = 1 ; i < n ; i++){
+                int ans = 0;
+                for(int j = i - 1 ; j >= 0 ;j--){
+                    if(nums[i] > nums[j]){
+                        ans = max(ans , dp[j]);
+                    }
+                }  
+                dp[i] = ans + 1;
+                maxi = max(maxi , dp[i]);
+            }
+            return maxi;
     }
-    return maxi;
-}
 };
