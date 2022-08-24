5b976196fc0533dd6cd01e8d7d58ca47bad91911
Parth Arora <105902832+CodeChefer@users.noreply.github.com>
2022-08-24 22:27:26 +0530

    Time: 3 ms (37.44%), Space: 6 MB (68.59%) - LeetHub

--- 2160-minimum-sum-of-four-digit-number-after-splitting-digits/2160-minimum-sum-of-four-digit-number-after-splitting-digits.cpp
+++ 2160-minimum-sum-of-four-digit-number-after-splitting-digits/2160-minimum-sum-of-four-digit-number-after-splitting-digits.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
     int minimumSum(int num) {
-        priority_queue<int , vector<int> , greater<int>> pq;
+        vector<int> ar;
         while(num > 0){
-            pq.push(num % 10);
-            num/=10;
+            ar.push_back(num%10);
+            num /= 10;
         }
-        int sum1 =0,sum2 = 0;
-        sum1 += pq.top() * 10;
-        pq.pop();
-        sum2 += pq.top() * 10;
-        pq.pop();
-        sum1 += pq.top();
-        pq.pop();
-        sum2 += pq.top();
-        pq.pop();
+        sort(ar.begin(),ar.end());
+        int sum1 =0 , sum2 = 0;
+        sum1 = ar[0]* 10;
+        sum2 = ar[1] * 10;
+        sum1 += ar[2];
+        sum2 += ar[3];
         return sum1 + sum2;
     }
 };
