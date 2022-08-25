75c609cef1f273d5aceb59c3762f6f02d4536d8b
Parth Arora <105902832+CodeChefer@users.noreply.github.com>
2022-08-25 09:40:58 +0530

    Time: 3 ms (38.52%), Space: 5.9 MB (51.36%) - LeetHub

---
+++ 1323-maximum-69-number/1323-maximum-69-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maximum69Number (int n) {
+        string s = to_string(n);
+        for(int i = 0 ; i < s.length() ; i++){
+            if(s[i] == '6'){
+                s[i] = '9';
+                break;
+            }
+        }
+        return stoi(s);
+    }
+};
