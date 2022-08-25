eed96ea0ae7495be4f119a61c4d78fc901fb8fa0
Parth Arora <105902832+CodeChefer@users.noreply.github.com>
2022-08-25 19:54:05 +0530

    Time: 149 ms (11.58%), Space: 30.3 MB (9.82%) - LeetHub

--- 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.cpp
+++ 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.cpp
@@ -1,22 +1,26 @@
 class Solution {
 public:
-    int countConsistentStrings(string allowed, vector<string>& words) {
-        unordered_set<char> myset;
-        for(int i = 0 ; i < allowed.size() ;i++){
-            myset.insert(allowed[i]);
-        }
-        int ans = 0;
-        for(int i = 0 ; i < words.size() ;i ++){
-            for(int j = 0 ; j  < words[i].size() ;j++){
-                if(myset.count(words[i][j]) == 0){
-                    ans -= 1;
-                    // cout << true << endl;
-                    break;
-                }
-            }
-        ans+=1;
-            
-        }
-        return ans;
+   int countConsistentStrings(string allowed, vector<string> &words)
+{
+    unordered_set<char> myset;
+    for (int i = 0; i < allowed.size(); i++)
+    {
+        myset.insert(allowed[i]);
     }
+    int ans = 0;
+    for (int i = 0; i < words.size(); i++)
+    {
+        for (int j = 0; j < words[i].size(); j++)
+        {
+            if (myset.count(words[i][j]) == 0)
+            {
+                ans -= 1;
+                cout << true << endl;
+                break;
+            }
+        }
+        ans += 1;
+    }
+    return ans;
+}
 };
