1e9f01c8d58a14a72c052a69f175465b90f4d24d
Parth Arora <105902832+CodeChefer@users.noreply.github.com>
2022-09-01 19:26:39 +0530

    Time: 482 ms (39.57%), Space: 102.5 MB (79.87%) - LeetHub

--- 575-distribute-candies/575-distribute-candies.cpp
+++ 575-distribute-candies/575-distribute-candies.cpp
@@ -3,12 +3,12 @@ public:
     int distributeCandies(vector<int>& ar) {
         unordered_set<int> s1;
         int n = ar.size();
+        int target = n / 2;
         for(int i = 0 ; i < n ;i++){
             s1.insert(ar[i]);
-        }
-        int target = n / 2;
-        if(s1.size() > target){
-            return target;
+            if(s1.size() > target){
+                return target;
+            }
         }
         return s1.size();
         
