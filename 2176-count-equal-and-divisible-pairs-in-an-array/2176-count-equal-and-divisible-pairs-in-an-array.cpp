0fc4604cf180d1d72c3f6b015d55701a156596ec
Parth Arora <105902832+CodeChefer@users.noreply.github.com>
2022-09-03 23:38:01 +0530

    Time: 29 ms (9.90%), Space: 11.8 MB (70.78%) - LeetHub

--- 2176-count-equal-and-divisible-pairs-in-an-array/2176-count-equal-and-divisible-pairs-in-an-array.cpp
+++ 2176-count-equal-and-divisible-pairs-in-an-array/2176-count-equal-and-divisible-pairs-in-an-array.cpp
@@ -1,30 +1,40 @@
 class Solution {
 public:
     int countPairs(vector<int>& ar, int k) {
-        unordered_map<int , vector<int>> mymap;
-        int n = ar.size();
-        for(int i =0 ; i < n ;i++){
-            mymap[ar[i]].push_back(i);
-        }
+//         unordered_map<int , vector<int>> mymap;
+//         int n = ar.size();
+//         for(int i =0 ; i < n ;i++){
+//             mymap[ar[i]].push_back(i);
+//         }
+        
+//         int ans = 0;
+        
+//         auto it = mymap.begin();
+//         while(it != mymap.end()){
+            
+//             int n = it->second.size();
+//             if(n >= 2){
+//                 for(int i =0 ; i < it->second.size() - 1;i++){
+//                     for(int j = i + 1 ; j < it->second.size() ; j++){
+//                         if((it->second[i] * it->second[j]) % k == 0){
+//                             ans++;        
+//                         }
+//                     }
+//                 }
+//             }
+            
+//             it++;
+//         }
         
         int ans = 0;
         
-        auto it = mymap.begin();
-        while(it != mymap.end()){
-            
-            int n = it->second.size();
-            if(n >= 2){
-                for(int i =0 ; i < it->second.size() - 1;i++){
-                    for(int j = i + 1 ; j < it->second.size() ; j++){
-                        if((it->second[i] * it->second[j]) % k == 0){
-                            ans++;        
-                        }
-                    }
-                }
+        for(int i = 0 ; i < ar.size() - 1;i++){
+            for(int j = i +1 ; j < ar.size() ; j++){
+                if(ar[i] == ar[j] && (i * j) % k == 0)
+                    ans++;
             }
-            
-            it++;
         }
+        
         
         return ans;
     }
