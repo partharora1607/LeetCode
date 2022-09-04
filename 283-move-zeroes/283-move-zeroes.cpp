e8acc16cb84e1162ce9a421bfe1df15a16731338
Parth Arora <105902832+CodeChefer@users.noreply.github.com>
2022-09-04 22:11:26 +0530

    Time: 25 ms (88.63%), Space: 19.3 MB (13.69%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -3,13 +3,11 @@ public:
     void moveZeroes(vector<int>& ar) {
         int n = ar.size();
         int index = 0;
-        for(int i = 0 ; i < n ;i++){
+        for(int i = 0 ; i < n ; i++){
             if(ar[i] != 0){
-                ar[index++] = ar[i];
+                swap(ar[index++] , ar[i]);
             }
         }
-        for(int i = index ; i < n ;i++){
-            ar[i] = 0;
-        }
+        
     }
 };
