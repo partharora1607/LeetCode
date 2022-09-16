fde4cee681707ef6cf8efc04bd5c59cb21acf831
Parth Arora <105902832+CodeChefer@users.noreply.github.com>
2022-09-17 00:25:14 +0530

    Time: 31 ms (5.50%), Space: 10.3 MB (40.41%) - LeetHub

--- 414-third-maximum-number/414-third-maximum-number.cpp
+++ 414-third-maximum-number/414-third-maximum-number.cpp
@@ -3,32 +3,20 @@ public:
     int thirdMax(vector<int>& ar) {
         unordered_map<int,int>mymap;
         int n = ar.size();
-        for(int i = 0 ; i < n  ; i ++){
+        int index = 0;
+        for(int i = 0; i < n ;i++){
+            if(mymap.count(ar[i]) == 0){
+                ar[index++]=  ar[i];
+            }
             mymap[ar[i]]++;
         }
-        int  index =0 ;
-        for(int i= 0 ; i < n; i ++){
-            if(mymap.count(ar[i])){
-                ar[index++] = ar[i];
-                mymap.erase(ar[i]);
-            }
-        }
-        
-        for(int i = 0 ; i < index ; i++){
-            cout << ar[i] << "  ";
-        }
-        
-        
-        
+                
         n = index;
-        
-        cout <<  "n" << " " << n << endl;
         priority_queue<int , vector<int> , greater<int>> pq;
         for(int i =0; i < n && i < 3 ; i++){
             pq.push(ar[i]);
         }
         if(pq.size() < 3){
-            cout << "hello" << endl;
             while(pq.size() != 1){
                 pq.pop();
             }
