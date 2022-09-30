0045a6eaf437be5ee390e296316879802c325264
Parth Arora <105902832+CodeChefer@users.noreply.github.com>
2022-09-30 19:26:56 +0530

    Time: 8 ms (79.67%), Space: 10.2 MB (44.82%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
@@ -1,28 +1,16 @@
 class Solution {
 public:
-    bool checkIfExist(vector<int>& ar) 
-    {
-        sort(ar.begin(), ar.end() , greater<int>());
+    bool checkIfExist(vector<int>& ar) {
         unordered_map<int,int> mymap;
-        int n = ar.size();
-        for(int i = 0 ; i < n ;i++){
-            if(ar[i] > 0)
-            {
-                if(mymap.count(2 * ar[i]))
-                {
-                    return true;
-                }
+        for(int i = 0 ; i < ar.size() ;i++){
+            if(mymap.count(ar[i] * 2)){
+                return true;
             }
-            else
+            else if(ar[i] % 2 == 0 && mymap.count(ar[i] / 2))
             {
-                if(ar[i] % 2 == 0 && mymap.count(ar[i] / 2)){
-                    return true;
-                }
+                return true;
             }
             mymap[ar[i]]++;
-        }
-        for(int i = 0 ; i  < ar.size() ; i++){
-            cout << ar[i] << " ";
         }
         return false;
     }
