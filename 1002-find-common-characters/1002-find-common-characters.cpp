be321677517a309bcb1b9575951d8d33d7d39aca
Parth Arora <105902832+CodeChefer@users.noreply.github.com>
2022-10-03 13:15:12 +0530

    Time: 46 ms (12.94%), Space: 11.6 MB (16.15%) - LeetHub

--- 1002-find-common-characters/1002-find-common-characters.cpp
+++ 1002-find-common-characters/1002-find-common-characters.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     vector<string> commonChars(vector<string> &words)
 {
+	vector<int> visited(26, 0);
 	vector<int> freq(26, INT_MAX);
-    vector<int> visited(26 , 0);
 	for (int i = 0; i < words.size(); i++)
 	{
 		string s = words[i];
 		unordered_map<char, int> mymap;
-		for (int j = 0; j < words[i].size(); j++)
+		for (int j = 0; j < s.size(); j++)
 		{
 			mymap[s[j]]++;
 		}
@@ -16,25 +16,28 @@ public:
 		while (it != mymap.end())
 		{
 			int val = it->first - 'a';
-			freq[val] = min(it->second, freq[val]);
-            visited[val]++;
+			visited[val]++;
+			freq[val] = min(freq[val], it->second);
 			it++;
 		}
 	}
-	vector<string> ans;
-	for (int i = 0; i < freq.size(); i++)
+
+	vector<string> res;
+
+	for (int i = 0; i < 26; i++)
 	{
 		if (freq[i] != INT_MAX && visited[i] == words.size())
 		{
 			while (freq[i] > 0)
 			{
-				string res = "";
-				res += i + 'a';
-				ans.push_back(res);
+				string s;
+				s += i + 'a';
+				res.push_back(s);
 				freq[i]--;
 			}
 		}
 	}
-	return ans;
+
+	return res;
 }
 };
