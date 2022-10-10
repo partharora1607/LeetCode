52a05ca1e6c8a9b6ce514782eea906708c41c750
Parth Arora <105902832+CodeChefer@users.noreply.github.com>
2022-10-10 17:39:54 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (45.53%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.cpp
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -6,26 +6,17 @@ public:
         if(n == 1){
             return "";
         }
-        int i = 0 , j = n - 1;
-        while(i < j)
-        {
-            int val = s[i] - 'a';
-            for(int k = 0 ; k < val ; k++)
-            {
-                char ch = k + 'a';
-                if(s[i] != ch)
-                {
-                    s[i] = ch;
-                    return s;
-                }    
+        for(int i = 0 ; i < n/2 ; i++){
+            if(s[i]!='a'){
+                s[i] = 'a';
+                return s;
             }
-            i++;
-            j--;
         }
-        if(s[0] == 'a'){
-            s[n - 1] = 'b'; 
+        if(s[n - 1] == 'a'){
+            s[n - 1] = 'b';
             return s;
         }
-        return ""; 
+        return "";
+        
     }
 };
