645f2d360a3124c935fea86852ffa27a9797a62e
Parth Arora <105902832+CodeChefer@users.noreply.github.com>
2022-10-14 22:36:11 +0530

    Time: 19 ms (71.25%), Space: 12.6 MB (80.06%) - LeetHub

--- 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
@@ -1,14 +1,9 @@
 class Solution {
 public:
     vector<vector<int>> groupThePeople(vector<int>& ar) {
-        
         unordered_map<int , vector<int>> mymap;
-        
         vector<vector<int>> res;
-       
-        int n = ar.size();
-        
-        for(int i =0 ; i < n ; i++){
+        for(int i =0 ; i < ar.size() ; i++){
             int size = ar[i];
             mymap[size].push_back(i);
             if(mymap[size].size() == size)
@@ -17,7 +12,6 @@ public:
                 mymap[size] = {};
             }
         }
-        
         return res;
     }
 };
